26e5677b5985272d2c83203e076a64d51f6b890b
Malinin Andrey <malinin.andrey@huawei-partners.com>
2023-12-05 00:40:50 +0300

    Improved mem translation

--- cmake/Generation.cmake
+++ cmake/Generation.cmake
@@ -13,7 +13,7 @@ function(gen_file)
     set(DEPENDS_LIST ${ARG_GENERATOR} ${ARG_TEMPLATE})
 	add_custom_command(OUTPUT ${ARG_OUTPUT}
         COMMENT "Generate file for ${ARG_TEMPLATE}"
-        COMMAND ${Ruby_EXECUTABLE} ${ARG_GENERATOR} --root ${PROJECT_ROOT} --template ${ARG_TEMPLATE} --output ${ARG_OUTPUT}
+        COMMAND ${RUBY_EXECUTABLE} ${ARG_GENERATOR} --root ${PROJECT_ROOT} --template ${ARG_TEMPLATE} --output ${ARG_OUTPUT}
         DEPENDS ${DEPENDS_LIST}
     )
 endfunction()
