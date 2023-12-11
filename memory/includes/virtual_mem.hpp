1503d8c1ed127566d670f93a50170da31766d74e
Malinin Andrey <malinin.andrey@huawei-partners.com>
2023-12-11 23:31:46 +0300

    Added translation lookaside buffer

--- memory/includes/virtual_mem.hpp
+++ memory/includes/virtual_mem.hpp
@@ -12,6 +12,7 @@ namespace simulator::mem {
 class VirtualMem final {
 public:
     static constexpr size_t PTE_SIZE = 8;
+    static constexpr size_t TLB_SIZE = 4096;
     NO_COPY_SEMANTIC(VirtualMem)
     NO_MOVE_SEMANTIC(VirtualMem)
 
@@ -32,14 +33,17 @@ public:
 private:
     VirtualMem();
     ~VirtualMem();
-    uint64_t GetPageIdByAddress(uintptr_t addr) const;
-    uint64_t GetPageOffsetByAddress(uintptr_t addr) const;
+    inline uint64_t GetPageOffsetByAddress(uintptr_t addr) const;
+    inline uint64_t RemoveOffset(uintptr_t addr) const;
     uint64_t PageLookUp(uint32_t vpn0, uint32_t vpn1, uint32_t vpn2, uint32_t vpn3);
+    inline uintptr_t CheckInTlb(int64_t id, uintptr_t vaddr);
+    inline void PushToTlb(int64_t vpn0, uintptr_t vaddr, uintptr_t paddr);
     uint8_t *GetPhysAddrWithAllocation(uintptr_t vaddr);
-    bool IsVirtAddrCanonical(uintptr_t vaddr) const;
+    inline bool IsVirtAddrCanonical(uintptr_t vaddr) const;
     uintptr_t GetPointer(uint64_t page_id, uint64_t page_offset) const;
-    void validateElfHeader(const GElf_Ehdr &ehdr) const;
+    void ValidateElfHeader(const GElf_Ehdr &ehdr) const;
 
+    std::vector<std::pair<int64_t, uintptr_t>> tlb_;
     PhysMem *ram_ = nullptr;
 };
 }  // namespace simulator::mem
